9b2bc1b31a6d0b3e07fcfc87ad9b8bdaaeca0c28
Cody <byrnes.cody@gmail.com>
2017-07-15 15:41:51 -0700

    Added test info

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ var ex = new j5.Expander("MCP23017")
 var p = {
   d0:1,  // Data Bus
   d1:2,
-  d2:3, 
-  d3:4, 
+  d2:3,
+  d3:4,
   d4:5,
   d5:6,
   d6:7,
@@ -62,7 +62,7 @@ async function setreg(address, data){
   ex.digitalWrite(p.wr, 0)
   await delay('2u')
   ex.digitalWrite(p.wr, 1)
-  ex.digitalWrite(p.cs, 1) 
+  ex.digitalWrite(p.cs, 1)
 }
 
 async function setup(){
@@ -78,12 +78,57 @@ async function setup(){
   ex.digitalWrite(p.ic, 0)
   await delay('10m')
   ex.digitalWrite(p.cs, 1)
-  await delay('10m)
+  await delay('10m')
 
-   
+  setreg(0x22, 0x00); // LFO off
+  setreg(0x27, 0x00); // Note off (channel 0)
+  setreg(0x28, 0x01); // Note off (channel 1)
+  setreg(0x28, 0x02); // Note off (channel 2)
+  setreg(0x28, 0x04); // Note off (channel 3)
+  setreg(0x28, 0x05); // Note off (channel 4)
+  setreg(0x28, 0x06); // Note off (channel 5)
+  setreg(0x2B, 0x00); // DAC off
+  setreg(0x30, 0x71); //
+  setreg(0x34, 0x0D); //
+  setreg(0x38, 0x33); //
+  setreg(0x3C, 0x01); // DT1/MUL
+  setreg(0x40, 0x23); //
+  setreg(0x44, 0x2D); //
+  setreg(0x48, 0x26); //
+  setreg(0x4C, 0x00); // Total level
+  setreg(0x50, 0x5F); //
+  setreg(0x54, 0x99); //
+  setreg(0x58, 0x5F); //
+  setreg(0x5C, 0x94); // RS/AR
+  setreg(0x60, 0x05); //
+  setreg(0x64, 0x05); //
+  setreg(0x68, 0x05); //
+  setreg(0x6C, 0x07); // AM/D1R
+  setreg(0x70, 0x02); //
+  setreg(0x74, 0x02); //
+  setreg(0x78, 0x02); //
+  setreg(0x7C, 0x02); // D2R
+  setreg(0x80, 0x11); //
+  setreg(0x84, 0x11); //
+  setreg(0x88, 0x11); //
+  setreg(0x8C, 0xA6); // D1L/RR
+  setreg(0x90, 0x00); //
+  setreg(0x94, 0x00); //
+  setreg(0x98, 0x00); //
+  setreg(0x9C, 0x00); // Proprietary
+  setreg(0xB0, 0x32); // Feedback/algorithm
+  setreg(0xB4, 0xC0); // Both speakers on
+  setreg(0x28, 0x00); // Key off
+  setreg(0xA4, 0x22); //
+  setreg(0xA0, 0x69); // Set frequency
 
 }
 
-board.on('ready', function() {
-  
+board.on('ready', async function() {
+  while(true) {
+    await delay('1s');
+    setreg(0x28, 0xF0); // Key on
+    await delay('1s');
+    setreg(0x28, 0x00); // Key off
+  }
 });
